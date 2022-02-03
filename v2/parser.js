04af4a716f2d4784cd92734abd7452e6d12f073a
exmuh <ex20070129@gmail.com>
2022-02-04 03:02:00 +0900

    new

--- v2/parser.js
+++ v2/parser.js
@@ -9,7 +9,8 @@ var parser;
     }
     parser.virtualState = virtualState;
     class virtualDom {
-        constructor(type_, state_, children_) {
+        constructor(type_, state_, children_, text_) {
+            this.text = text_;
             this.type = type_;
             this.state = state_;
             this.children = children_;
@@ -31,8 +32,56 @@ var parser;
         const tagName = element.tagName;
         const attributes = parser.parseAttributes(element.attributes);
         const children = [];
+        const text = element.innerHTML;
         for (let i = 0; i < element.children.length; i++)
             children.push(parser.parseElement(element.children[i]));
-        return new virtualDom(tagName, attributes, children);
+        return new virtualDom(tagName, attributes, children, text);
+    };
+    parser.createChild = (dom, target) => {
+        const myNode = document.createElement(dom.type);
+        //last dom
+        if (dom.children.length === 0) {
+            const textNode = document.createTextNode(dom.text);
+            myNode.appendChild(textNode);
+        }
+        target.appendChild(myNode);
+        dom.children.map(element => parser.createChild(element, target.children[0]));
+    };
+    parser.renderAttributes = (lastProps, changedProps, target) => {
+        changedProps.forEach((element) => {
+            const lastData = lastProps.find(prop => prop.type === element.type);
+            const changedData = element;
+            //add new data
+            if (lastData == undefined)
+                target.setAttribute(changedData.type, changedData.value);
+            //change data
+            else if (lastData.value !== changedData.value)
+                target.setAttribute(changedData.type, changedData.value);
+        });
+        lastProps.forEach((element) => {
+            const lastData = element;
+            const changedData = changedProps.find(prop => prop.type === element.type);
+            //delete data
+            if (changedData == undefined)
+                target.removeAttribute(lastData.type);
+        });
+    };
+    parser.render = (lastDom, changedDom, target) => {
+        //delete
+        if (changedDom == undefined) {
+            target.parentElement?.removeChild(target);
+        }
+        //add
+        if (lastDom == undefined) {
+            parser.createChild(changedDom, target.parentElement);
+        }
+        //same tag
+        if (lastDom.type === changedDom.type) {
+            parser.renderAttributes(lastDom.state, changedDom.state, target);
+        }
+        //different tag
+        if (lastDom.type !== changedDom.type) {
+            parser.createChild(changedDom, target.parentElement);
+        }
     };
 })(parser || (parser = {}));
